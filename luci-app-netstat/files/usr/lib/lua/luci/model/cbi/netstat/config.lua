058a2f5ab4c948a71b5b87a4e6110e28786e0402
Doty The Cat <dotywrt@gmail.com>
2025-07-20 10:50:50 +0800

    Update config.lua

--- luci-app-netstat/files/usr/lib/lua/luci/model/cbi/netstat/config.lua
+++ luci-app-netstat/files/usr/lib/lua/luci/model/cbi/netstat/config.lua
@@ -1,12 +1,18 @@
 local net = require "luci.model.network"
 
-local m = Map("netstats", translate("Netstat Config"),
+local m = Map("netstats", translate("Netstat"),
     translate("Select your preferred primary WAN interface."))
 
-local s = m:section(TypedSection, "config", "")
+-- Actual config section, with a title
+local s = m:section(TypedSection, "config", translate("Settings"))
 s.anonymous = true
 
-local iface = s:option(ListValue, "prefer", translate("Preferred WAN Interface"))
+local iface = s:option(ListValue, "prefer", translate("WAN Interface"))
+iface.description = translate(
+    "Choose the interface used to detect WAN traffic. " ..
+    "Leave it as Auto detect unless you want to override. " ..
+    "In case Auto detect selects the wrong interface, please use manual configuration."
+)
 
 local netm = net.init()
 local devs = netm:get_interfaces()
